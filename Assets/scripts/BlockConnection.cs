a829e2db9a34e36d89bbcfb57d04e097846bea84
Your Name <you@example.com>
2018-03-13 20:28:41 +0100

    initial

---
+++ Assets/scripts/BlockConnection.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BlockConnection : MonoBehaviour {
+
+    List<Block> ConnectedBlocks = new List<Block>();
+    private BlockColor? CurrentColor;
+    private Board Board;
+    private LineRenderer LineRenderer;
+
+    public event Action<int> OnConnection;
+
+    void Awake()
+    {
+        Board = FindObjectOfType<Board>();
+        LineRenderer = GetComponent<LineRenderer>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!Input.GetMouseButton(0))
+        {
+            FinishConnection();
+        }
+	}
+
+
+
+    public void Connect(Block block)
+    {
+        if (!Input.GetMouseButton(0)) return;
+        if (ConnectedBlocks.Contains(block)) return;
+
+        // limiting connecting block only to ones with the same color
+        if (!CurrentColor.HasValue)
+        {
+            CurrentColor = block.Color;
+        }
+        if (CurrentColor != block.Color) return;
+
+        if (ConnectedBlocks.Count() >= 1 && !ConnectedBlocks.Last().IsNeighbour(block)) return;
+
+        block.IsConnected = true;
+        ConnectedBlocks.Add(block);
+
+        RefreshConnection();
+    }
+
+    private void FinishConnection()
+    {
+        ConnectedBlocks
+            .ForEach(block => block.IsConnected = false);
+
+        if (ConnectedBlocks.Count >= 3)
+        {
+            if (OnConnection != null)
+            {
+                OnConnection.Invoke(ConnectedBlocks.Count);
+            }
+
+            Board.RemoveBlocks(ConnectedBlocks);
+            Board.RefreshBlocks();
+
+        }
+
+        ConnectedBlocks.Clear();
+
+        CurrentColor = null;
+        RefreshConnection();
+    }
+
+    private void RefreshConnection()
+    {
+        var points = ConnectedBlocks
+            .Select(block => Board.GetBlockPosition(block.X, block.Y))
+            .Select(position => (Vector3)position + Vector3.back * 2f)
+            .ToArray();
+
+        LineRenderer.positionCount = points.Length;
+        LineRenderer.SetPositions(points);
+
+    }
+}
---
+++ Assets/scripts/MenuUserInterface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuUserInterface : MonoBehaviour {
+
+    [SerializeField]
+    Number LastGameScore;
+
+    [SerializeField]
+    Number RecordScore;
+
+	// Use this for initialization
+	void Start () {
+        LastGameScore.Value = PlayerPrefs.GetInt(PlayerPrefsConst.LastGameScore, 0);
+        RecordScore.Value = PlayerPrefs.GetInt(PlayerPrefsConst.RecordScore, 0);
+	}
+
+    public void LoadGame()
+    {
+        FindObjectOfType<ScreenChanger>().ChangeScene(SceneNames.Game);
+    }
+}
